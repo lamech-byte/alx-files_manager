67fddca023b50d0872811244f841726d6ad0c5d2
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-09-07 07:08:41 +0300

    redis.js

---
+++ utils/redis.js
@@ -0,0 +1,70 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    // Create a new Redis client
+    this.client = redis.createClient();
+
+    // Listen for any errors in the Redis client and log them
+    this.client.on('error', (error) => {
+      console.error(`Redis Client Error: ${error}`);
+    });
+  }
+
+  // Check if the connection to Redis is alive
+  isAlive() {
+    return this.client.connected;
+  }
+
+  // Get a value from Redis based on a key
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  // Set a key-value pair in Redis with an optional expiration time in seconds
+  async set(key, value, durationInSeconds = 0) {
+    return new Promise((resolve, reject) => {
+      if (durationInSeconds === 0) {
+        this.client.set(key, value, (err, reply) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(reply);
+          }
+        });
+      } else {
+        this.client.setex(key, durationInSeconds, value, (err, reply) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(reply);
+          }
+        });
+      }
+    });
+  }
+
+  // Delete a key from Redis
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Export an instance of the RedisClient class
+export default new RedisClient();
