948218691fcc1cfd6bb8edcf40dcea13484f21e2
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-09-08 09:04:11 +0300

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // Import necessary modules
-import express from 'express';
-import routes from './routes/index';
+const express = require('express');
 const app = express();
 
 // Define the port to listen on
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+// Load routes from the routes/index.js file
+const routes = require('./routes');
 
 // Use the routes in the Express app
-app.use(express.json());
-
-routes(app);
+app.use('/', routes);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
