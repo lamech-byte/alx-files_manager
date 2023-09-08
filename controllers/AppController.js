8980f6579274ca00a4c345dfddd8e27fb83ef64f
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-09-08 08:59:58 +0300

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,28 +1,22 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-const AppController = {
+class AppController {
   // Endpoint to check the status of Redis and the DB
-  getStatus: async (req, res) => {
-    const redisStatus = await redisClient.isAlive();
-    const dbStatus = await dbClient.isAlive();
-    
-    return res.status(200).json({
-      redis: redisStatus,
-      db: dbStatus,
-    });
-  },
+  static getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+
+    res.status(200).send({ redis: redisStatus, db: dbStatus });
+  }
 
   // Endpoint to get the number of users and files in DB
-  getStats: async (req, res) => {
-    const numUsers = await dbClient.nbUsers();
-    const numFiles = await dbClient.nbFiles();
-    
-    return res.status(200).json({
-      users: numUsers,
-      files: numFiles,
-    });
-  },
-};
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
 
-module.exports = AppController;
+    res.status(200).send({ users, files });
+  }
+}
+
+export default AppController;
