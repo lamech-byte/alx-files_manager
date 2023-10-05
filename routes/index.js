679ff847525ba1cef32bd0a500f6d4cfb97889cc
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-10-05 10:14:44 +0300

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,27 @@
-/*
- * Create endpoints for our app
- */
-
-import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-function routes(app) {
-  const router = express.Router();
+const express = require('express');
 
-  // Attach the router to the app
-  app.use('/', router);
+const router = (app) => {
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
 
-  // Define an endpoint to get the status
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+  paths.get('/status', ((request, response) => AppController.getStatus(request, response)));
+  paths.get('/stats', ((request, response) => AppController.getStats(request, response)));
+  paths.post('/users', ((request, response) => UsersController.postNew(request, response)));
+  paths.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+  paths.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+  paths.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
+  paths.post('/files', ((request, response) => FilesController.postUpload(request, response)));
+  paths.get('/files/:id', ((request, response) => FilesController.getShow(request, response)));
+  paths.get('/files', ((request, response) => FilesController.getIndex(request, response)));
+  paths.put('/files/:id/publish', ((request, response) => FilesController.putPublish(request, response)));
+  paths.put('/files/:id/unpublish', ((request, response) => FilesController.putUnpublish(request, response)));
+  paths.get('/files/:id/data', ((request, response) => FilesController.getFile(request, response)));
+};
 
-  // Define an endpoint to get statistics
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
-
-  // Define an endpoint to create a new user
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
-
-  // Define an endpoint to sign in user
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
-
-  // Define an endpoint to sign out the user
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
-
-  // Define an endpoint to retrieve the user based on the token
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
-
-  // Define an endpoint files
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
-
-  // Define an endpoint to retrieve the file
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
-
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
-
-  // Define a publish/unpublish route
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
-
-  // Define an unpublished route
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
-
-  router.get('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
-}
-
-export default routes;
+export default router;
